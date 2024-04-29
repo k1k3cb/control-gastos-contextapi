18e299bc4fecb5ffedd0d0ee1ae30acc15b6067a
Enrique Comesaña <enriquecbar@gmail.com>
2024-04-30 01:20:45 +0200

    date picker

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import DatePicker from 'react-date-picker';
+import { categories } from '../data/categories';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
+
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
   return (
-    <div>ExpenseForm</div>
-  )
-}
+    <form className='space-y-5'>
+      <legend className='uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2'>
+        Nuevo gasto
+      </legend>
 
-export default ExpenseForm
+      <div className='flex flex-col gP-2'>
+        <label htmlFor='expenseName' className='text-xl'>
+          Nombre Gasto:
+        </label>
+        <input
+          type='text'
+          name='expenseName'
+          id='expenseName'
+          placeholder='Añade el nombre del gasto '
+          className='bg-slate-100 p-2'
+        />
+      </div>
+
+      <div className='flex flex-col gP-2'>
+        <label htmlFor='amout' className='text-xl'>
+          Cantidad:
+        </label>
+        <input
+          type='number'
+          name='amout'
+          id='amout'
+          placeholder='Añade la cantidad del gasto: ej. 300 '
+          className='bg-slate-100 p-2'
+        />
+      </div>
+
+      <div className='flex flex-col gP-2'>
+        <label htmlFor='category' className='text-xl'>
+          Categoría:
+        </label>
+        <select name='category' id='category' className='bg-slate-100 p-2'>
+          <option value=''>-- Seleccione --</option>
+          {categories.map(category => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className='flex flex-col gP-2'>
+        <label htmlFor='amout' className='text-xl'>
+          Fecha gasto:
+        </label>
+        <DatePicker className='bg-slate-100 p-2 border-0' />
+      </div>
+      <input
+        type='submit'
+        className='bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg'
+        value={'Añadir Gasto'}
+      />
+    </form>
+  );
+};
+
+export default ExpenseForm;
